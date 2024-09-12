ad95070fadb703b982df4ff90ffb4e6cc8f89ef1
akakou <akakou571@gmail.com>
2024-09-13 01:13:50 +0900

    feat: example of ctstream using crt.sh

---
+++ example/crtsh/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/akakou/ctstream/core"
+	"github.com/akakou/ctstream/thirdparty/crtsh"
+	ctx509 "github.com/google/certificate-transparency-go/x509"
+)
+
+func main() {
+	core.DefaultEpochSleep = time.Second * 10
+
+	m, err := crtsh.DefaultCTsStream([]string{
+		"test2.ochano.co",
+	}, context.Background())
+
+	if err != nil {
+		fmt.Printf("Failed to create new ctstream: ")
+		os.Exit(1)
+	}
+
+	m.Start(func(cert *ctx509.Certificate, i int, opt any, err error) {
+		if err != nil {
+			fmt.Printf("Failed to fetch %v: \n", err)
+		}
+
+		params := opt.(*crtsh.CrtshCTParams)
+
+		fmt.Printf("%v: %v (target: %v)\n", params.Index, cert.DNSNames, params.Client.Domain)
+	})
+
+	go func() {
+		time.Sleep(40 * time.Second)
+		m.Stop()
+	}()
+
+	m.Await()
+}
